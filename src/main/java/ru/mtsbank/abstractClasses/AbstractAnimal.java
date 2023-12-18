7dfcce9f32a17c12a1dc0a3bede72261d4caf90b
Artyom2302 <89930657+Artyom2302@users.noreply.github.com>
2023-12-18 08:59:27 +0300

    ДЗ№3

--- src/main/java/ru/mtsbank/abstractClasses/AbstractAnimal.java
+++ src/main/java/ru/mtsbank/abstractClasses/AbstractAnimal.java
@@ -5,8 +5,6 @@ import ru.mtsbank.Animal;
 import java.math.BigDecimal;
 
 public abstract class AbstractAnimal implements Animal {
-    AbstractAnimal(){
-    }
     protected String breed;
     protected String name;
     protected BigDecimal cost;
