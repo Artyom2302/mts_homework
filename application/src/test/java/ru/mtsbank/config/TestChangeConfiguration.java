ca2344e4a77e49bbd389ed1b44341cffef993e0b
Artyom2302 <89930657+Artyom2302@users.noreply.github.com>
2024-02-18 12:36:53 +0300

    дз 8

---
+++ application/src/test/java/ru/mtsbank/config/TestChangeConfiguration.java
@@ -0,0 +1,17 @@
+package ru.mtsbank.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import ru.mtsbank.service.CreateAnimalServiceImpl;
+
+@TestConfiguration
+
+public class TestAutoConfiguration {
+
+
+
+}
